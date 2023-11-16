62c79a3543fdab713510b8ce4997c723510fe71b
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:21:25 +0700

    Belajar OperatorAritmatika

---
+++ OperatorAritmatika.php
@@ -0,0 +1,34 @@
+<?php
+$a = 10;
+$b= 5;
+
+$result = 10 + 10;
+var_dump ($result);
+
+$result = $a +$b;
+var_dump($result);
+
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+$resultPositive = +$result;
+var_dump($resultPositive);
+
+$resultModulo = 100 % 3 ;
+var_dump($resultModulo);
+
+$resultpangkat = 25**3;
+var_dump($resultpangkat);
+
+$resultpengurangan = $a - $b;
+var_dump($resultpengurangan);
+
+$resultperkalian = $a * $b;
+var_dump($resultperkalian);
+
+$resultpembagian = $a / $b;
+var_dump($resultpembagian);
+
+
+
+
