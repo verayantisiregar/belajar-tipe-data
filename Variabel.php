65b819c733d56ca43c6a706af873de277983ea36
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:19:34 +0700

    Belajar Variabel

---
+++ Variabel.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "Vera";
+$age ="23";
+
+
+echo "Name:";
+echo $name;
+echo "\n";
+echo "Age:";
+echo $age;
+echo "\n";
+
+$Contoh = "Vera";
+$Example = "Siregar";
+
+echo "Contoh: ";
+echo $Example;;
+echo "\n";
+
+echo "Vera:";
+echo $Example = "Siregar";
+echo "\n";
