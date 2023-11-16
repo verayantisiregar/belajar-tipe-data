b77087d358ca90843231be88a4e56da555aec91c
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:25:18 +0700

    Belajar Operator Penugasan

---
+++ OperatorPenugasan.php
@@ -0,0 +1,14 @@
+<?php
+//operator penugasan memberikn value pada sebuah variabel ditandai dengan sama dengN (=)
+
+$total = 0;
+
+$fruit = 5000;
+$chicken = 10000;
+$orangejuice = 5000;
+
+$total +=$fruit;
+$total +=$chicken;
+$total +=$orangejuice;
+
+ var_dump ($total);
