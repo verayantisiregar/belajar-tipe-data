f9ad3297770849f51ac5548932835ae53dd3db2e
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:34:59 +0700

    Belajar Constant

---
+++ Constant.php
@@ -0,0 +1,24 @@
+<?php
+
+define("AUTHOR", "Verayanti Siregar");
+define("APP_VERSION" , 100);
+define("Author", "Eko Regar" );
+define("app_version", 50);
+
+echo "Author : ";
+Echo AUTHOR;
+echo "\n";
+
+echo "App Version : ";
+Echo APP_VERSION;
+echo "\n";
+
+echo "Author : ";
+Echo Author;
+echo "\n";
+
+//constant tidak bisa di ubah
+
+echo "App Version : ";
+Echo app_version;
+echo "\n";
