da5eaa32e19071e3c1822d9396074388227a063d
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:38:53 +0700

    Belajar Operator Logika

---
+++ OperatorLogika.php
@@ -0,0 +1,15 @@
+<?php
+
+//operator boolean
+
+var_dump(true && true);
+var_dump(true && false);
+
+var_dump (true || true);
+var_dump (true || false);
+
+var_dump(true xor true);
+var_dump(true xor false);
+
+var_dump (!true);
+var_dump (!false);
