dd66fc13c89b667dd8bba192f9b54967a7cbd050
Verayanti Siregar <verayantisiregar123@gmail.com>
2023-11-16 23:14:53 +0700

    Belajar Tipe Data String

---
+++ TipeString.php
@@ -0,0 +1,19 @@
+<?php
+
+echo 'Name :' ;
+echo 'Verayanti Siregar' ;
+echo "\n";
+
+echo "Name :" ;
+echo "Verayanti\t Siregar\t Silali\n";
+echo "\n";
+
+Echo <<<VERA
+Ini adalah contoh string yang sangat panjang gak perlu ngetik ENTER secara manual, "bisa quote"juga
+
+VERA;
+
+Echo <<<'VERA'
+Ini adalah contoh string yang sangat panjang gak perlu ngetik ENTER secara manual, "bisa quote"juga
+
+VERA;
